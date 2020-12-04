d475c2d5224ac1973dd2779a3990e3e0b47b321e
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:15:48 -0600

    Add files via upload

---
+++ 14.-Elevar_a_una_potencia_python.py
@@ -0,0 +1,14 @@
+# Este programa muestra como elevar a una potencia un número.
+
+print("También mostramos un ejemplo de potencia: ")
+numero1 = 3.1416
+print("El numero 1 es igual a: ")
+print(numero1)
+potencia = numero1**2
+print("Su cuadrado es igual a: ")
+print(potencia)
+print("La potencia es una variable de tipo: ")
+print(type(potencia))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+# Este programa fue escrito por Emilio Carcaño Bringas
